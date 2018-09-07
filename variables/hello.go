576e93b617d7fe5da3bf5130f182e9c939411ab3
vinicio.ochoa <vinicio.ochoa@calidadsalud.gob.ec>
2018-09-06 21:25:04 -0500

    init

---
+++ variables/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("hello, world\n")
+	var resultado = add(10, 20)
+	var suma = strconv.Itoa(resultado)
+	fmt.Println("La suma es " + suma)
+}
