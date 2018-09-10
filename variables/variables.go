68db8f116576c926bf74730203bab9756c6eaee0
vinicio.ochoa <vinicio.ochoa@calidadsalud.gob.ec>
2018-09-09 21:57:47 -0500

    ejemplo variables

---
+++ variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+var expresionBooleana = false
+var numero int32 = 10
+
+func main() {
+	cadena := ""
+	b := true
+	expresionBooleana = true
+	fmt.Println(cadena)
+	fmt.Println(b)
+	fmt.Println(expresionBooleana)
+	fmt.Println(numero)
+}
